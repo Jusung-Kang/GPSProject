b9e797e8bf577c19dddf23cb6b86844ecd6efcb2
Jusung <kangjs30@gmail.com>
2025-10-24 08:01:05 +0900

    개발환경 구축중

--- backend/src/main/java/com/jskang/backend/BackendApplication.java
+++ backend/src/main/java/com/jskang/backend/BackendApplication.java
@@ -1,13 +1,13 @@
-package com.jskang.gpsproject;
+package com.jskang.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GpsProjectApplication {
+public class BackendApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(GpsProjectApplication.class, args);
+        SpringApplication.run(BackendApplication.class, args);
     }
 
 }
--- backend/src/test/java/com/jskang/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/jskang/backend/BackendApplicationTests.java
@@ -1,10 +1,10 @@
-package com.jskang.gpsproject;
+package com.jskang.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class GpsProjectApplicationTests {
+class BackendApplicationTests {
 
     @Test
     void contextLoads() {
